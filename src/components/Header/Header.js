d21dad6d72a9106eb405cf43918953b0cc804eb9
artursakirov <ketrirush123@gmail.com>
2023-05-30 14:23:25 +0500

    Fix Logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ export const Header = ({switchTheme, theme}) => {
 
     return (
         <div className='header'>
-            <img className='header-logo' src='/Logo.svg' alt='Logo'/>
+            <img className='header-logo' src='Logo.svg' alt='Logo'/>
             <img className='icon-swith-theme' onClick={switchTheme}
                  src={theme === 'light' ? 'IconLightTheme.svg' : 'IconDarkTheme.svg'} alt='SwitchTheme'/>
         </div>
